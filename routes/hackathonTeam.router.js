75ead4c9d36ef072afe36d60524356800bb4dea2
bitlaniineuron12 <mukesh@ineuron.ai>
2023-10-10 18:16:27 +0530

    hackathon team create

---
+++ routes/hackathonTeam.router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+// const authController = require('../controllers/auth.controller')
+// const authValidators = require('../middlewares/validators/auth.validator');
+const hackathonTeamValidator = require('../middlewares/validators/hackathonTeam.validator');
+const hackathonTeam = require('../controllers/hackathonTeam.controller');
+
+router.post('/create-team', hackathonTeamValidator.createTeamValidator, hackathonTeam.createTeam);
+router.post('/add-user-in-team', hackathonTeamValidator.addUserTeamValidator, hackathonTeam.addUserInTeam)
+
+module.exports = router;
--- routes/index.router.js
+++ routes/index.router.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 const healthCheckRouter = require('./healthCheck.router');
 const authRouter = require('./auth.router')
+const hackathonTeamRouter = require('./hackathonTeam.router')
 
-router.use('/health-check',healthCheckRouter);
+router.use('/health-check', healthCheckRouter);
 router.use('/auth', authRouter)
-
+router.use('/team', hackathonTeamRouter)
 module.exports = router;
