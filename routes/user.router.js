7301da5f83f40f69aaf005e4b71c30b0c8b7485b
ASKushwaha <ashishsingh@ineuron.ai>
2023-10-12 15:45:37 +0530

    routes change

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const fetchHackathon = require('../controllers/fetchHackathonUserSide.controller')
+const fetchHackathon = require('../controllers/fetchHackathonUserSide.controller');
+const fetchHackathonDetails = require('../controllers/fetchHackathonDetails.controller');
 const authValidators = require('../middlewares/validators/auth.validator')
 const security = require('../middlewares/security/auth.security')
 
+router.get('/hackathon-details',fetchHackathonDetails)
 router.get('/', fetchHackathon);
 
 
