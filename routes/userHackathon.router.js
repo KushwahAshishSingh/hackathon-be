22802bd5b7927633b992cdab36788fe300550e99
kesavavarma0325 <kesava@ineuron.ai>
2023-10-13 15:26:31 +0530

    fix security

--- routes/userHackathon.router.js
+++ routes/userHackathon.router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userHackathonController = require('../controllers/userHackathon.controller')
 const userHackathonValidator = require('../middlewares/validators/userHackathon.validator')
+const security = require('../middlewares/security/auth.security')
 
 router.post('/submit', security.verifyUser, userHackathonValidator.submitHackathon, userHackathonController.submitHackathon);
 
