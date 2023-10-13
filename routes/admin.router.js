040af570391de4ab84a9e3dcda350852d07bb7b5
ASKushwaha <ashishsingh@ineuron.ai>
2023-10-13 14:47:11 +0530

    evaluation

--- routes/admin.router.js
+++ routes/admin.router.js
@@ -5,6 +5,7 @@ const updateHackathon = require('../controllers/admin/updateHackathon')
 const deleteHackathon = require('../controllers/admin/deleteHackathon')
 const fetchHackathon = require('../controllers/admin/getHackathons')
 const listHackathonTeams = require('../controllers/admin/fetchTeamsSubmission')
+const evaluateHackathon = require('../controllers/admin/evaluateHackathon')
 const authValidators = require('../middlewares/validators/auth.validator')
 const security = require('../middlewares/security/auth.security')
 const hackathonValidator = require('../middlewares/validators/hackathon.validator')
@@ -14,7 +15,8 @@ router.patch('/', updateHackathon);
 router.delete('/', deleteHackathon);
 router.get('/', fetchHackathon);
 
-outer.post('/', security.verifyUser, security.isAdminCheck, hackathonValidator.createTeamValidator, security.verifyUser, security.isAdminCheck, createHackathon);
+router.post('/evaluate-hackathon', security.verifyUser, security.isAdminCheck, evaluateHackathon)
+router.post('/', security.verifyUser, security.isAdminCheck, hackathonValidator.createTeamValidator, security.verifyUser, security.isAdminCheck, createHackathon);
 router.patch('/', security.verifyUser, security.isAdminCheck, updateHackathon);
 router.delete('/', security.verifyUser, security.isAdminCheck, deleteHackathon);
 router.get('/fetch-teams', security.verifyUser, security.isAdminCheck, listHackathonTeams)
